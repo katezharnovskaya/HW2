996e6a005196d8fc280f1620d6982a443f997225
Ekaterina <firegirl-83@mail.ru>
2023-01-24 02:46:38 +0300

    add task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите трехзгначное число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int secondnumber = ((a/10 ) % 10 );
+
+Console.WriteLine ( "{0} Вторая цифра трехзначного чмсла ", secondnumber);

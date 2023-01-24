8e956d2773abde359dc3d39f8dbf6dd0c357e252
Ekaterina <firegirl-83@mail.ru>
2023-01-24 03:19:19 +0300

    add task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100)
+Console.WriteLine("Третьей цифры нет ");
+
+else
+{while (n > 999)
+{
+    n = n/10;
+}
+Console.WriteLine("{0} Третья цифра числа ", n % 10);
+}
